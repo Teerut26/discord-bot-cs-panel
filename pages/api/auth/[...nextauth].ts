7ed7bd1721ece1bf25754e51a315765399acee6e
Teerut26 <teerutsr@gmail.com>
2022-06-06 22:28:34 +0700

    add SECRET

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,8 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export default NextAuth({
-    // Configure one or more authentication providers
+    // Configure one or more authentication
+    secret: process.env.SECRET,
     providers: [
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID as string,
